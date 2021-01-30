85bd0506348a6ef93d3c49a49af07cb1f0f79a19
Emily Phee <ephee@uci.edu>
2021-01-30 11:28:27 -0800

    First commit after create a branch

--- nextjs-app/components/Main.js
+++ nextjs-app/components/Main.js
@@ -4,20 +4,21 @@ import CourtItem from '@components/CourtItem';
 import courtData from '../dummy-data.json';
 
 const Main = (props) => {
-    return ( 
-        <div className={styles.container}>
-            {props.children}
-            
-            {courtData.courts.map((court) => {
-                return (
-                    <CourtItem
-                        courtInfo={court}
-                    />
-                )
-            })}
+  return (
+    <div className={styles.container}>
+      {props.children}
 
-        </div>
-     );
+      {courtData.courts.map((court, index) => {
+        return (
+          <CourtItem
+            key={index}
+            courtInfo={court}
+          />
+        )
+      })}
+
+    </div>
+  );
 }
- 
+
 export default Main;
