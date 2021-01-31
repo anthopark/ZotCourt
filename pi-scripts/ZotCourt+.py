c310dc0293699f4956be176b10081df6d8ae36e0
andreAbtahi96 <sabtahi@uci.edu>
2021-01-30 17:19:18 -0800

    post request for when court is occupied is successful

--- pi-scripts/ZotCourt+.py
+++ pi-scripts/ZotCourt+.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO #to control pins!
 import time #for creating delays in between detection
+import requests
 
 '''
 resources used:
@@ -7,6 +8,8 @@ resources used:
 https://www.instructables.com/Vibration-Sensor-SW-420-Raspberry-Pi/
 
 https://www.geeksforgeeks.org/sleep-in-python/
+
+https://www.w3schools.com/python/ref_requests_response.asp
 
 '''
 
@@ -24,11 +27,29 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN)
 #END OF GPIO SET UP
 
+
 def callback(pin):
+
+    #When vibration detected, send packet to this url with json body below
+    vibrationDetectedPost = 'https://zotcourt.herokuapp.com/api/court/occupied'
+
+    #json body
+    courtDetected = {"courtId":1}
+
+    #When representing vacant, send packet to this url
+    vacancyDetectedPost = 'https://zotcourt.herokuapp.com/api/court/vacant'
+
+
+    
     if GPIO.input(pin):
+
+        x = requests.post(vibrationDetectedPost, data = courtDetected)
+        print(x)
         print "Movement Detected"
 
     else:
+        x = requests.post(vibrationDetectedPost, data = courtDetected)
+        print(x.status_code)
         print "Movement Detected"
 
 
@@ -40,8 +61,14 @@ GPIO.add_event_detect(pin, GPIO.BOTH, bouncetime=300)
 #function assigned to GPIO pin
 GPIO.add_event_callback(pin, callback)
 
+
+
 #While on, run
 while True:
     time.sleep(1)
+
+    
+
+
 
 
