94794372aee01820433490822cbb4e2db2bd7cdf
Emily Phee <ephee@uci.edu>
2021-01-30 17:43:15 -0800

    Add design for Header section

--- nextjs-app/components/Header.js
+++ nextjs-app/components/Header.js
@@ -1,21 +1,25 @@
 import styles from './css/Header.module.css';
 import SearchBar from '@components/SearchBar';
 import Logo from '@components/Logo';
+import HeaderText from '@components/HeaderText';
 
 
 
 const Header = (props) => {
-    return ( 
-        <div className={styles.container}>
-            <div className={styles.logoContainer}>
-                <Logo />
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <Logo />
+      </div>
 
-            <div className={styles.searchBarContainer}>
-                <SearchBar />
-            </div>
-        </div>
-     );
+
+
+      <div className={styles.searchBarContainer}>
+        <HeaderText />
+        <SearchBar />
+      </div>
+    </div>
+  );
 }
- 
+
 export default Header;
--- nextjs-app/components/Logo.js
+++ nextjs-app/components/Logo.js
@@ -1,11 +1,11 @@
 import styles from './css/Logo.module.css';
 
 const Logo = () => {
-    return ( 
-        <div className={styles.container}>
-
-        </div>
-     );
+  return (
+    <div className={styles.container}>
+      <img src="/zot-court-logo.svg" className={styles.zotLogo} />
+    </div>
+  );
 }
- 
+
 export default Logo;
