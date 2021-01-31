988eee65a8dbf44b4da9f915fa03f141c2131cbe
andreAbtahi96 <sabtahi@uci.edu>
2021-01-30 22:51:31 -0800

    post requests for both detection and vacancy are a success

--- pi-scripts/ZotCourt+.py
+++ pi-scripts/ZotCourt+.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO #to control pins!
 import time #for creating delays in between detection
 import requests
+import datetime
 
 '''
 resources used:
@@ -15,6 +16,8 @@ https://www.w3schools.com/python/ref_requests_response.asp
 
 #GPIO SET UP
 
+currentTime = 1
+
 #configure to your preference. Refer to gpio
 #pin 16/GPIO 23(pi) -> D0(sensor) 
 pin = 23   
@@ -28,34 +31,34 @@ GPIO.setup(pin, GPIO.IN)
 #END OF GPIO SET UP
 
 
+
+#When vibration detected, send packet to this url with json body below
+vibrationDetectedPost = 'https://zotcourt.herokuapp.com/api/court/occupied'
+
+#json body
+courtDetected = {"courtId":1}
+
+#When representing vacant, send packet to this url
+vacancyDetectedPost = 'https://zotcourt.herokuapp.com/api/court/vacant'
+
+
+
 def callback(pin):
-
-    #When vibration detected, send packet to this url with json body below
-    vibrationDetectedPost = 'https://zotcourt.herokuapp.com/api/court/occupied'
-
-    #json body
-    courtDetected = {"courtId":1}
-
-    #When representing vacant, send packet to this url
-    vacancyDetectedPost = 'https://zotcourt.herokuapp.com/api/court/vacant'
+    global currentTime
 
 
-    
-    if GPIO.input(pin):
-
-        x = requests.post(vibrationDetectedPost, data = courtDetected)
-        print(x)
-        print "Movement Detected"
-
-    else:
-        x = requests.post(vibrationDetectedPost, data = courtDetected)
-        print(x.status_code)
-        print "Movement Detected"
-
+    #vibration detected
+    x = requests.post(vibrationDetectedPost, data = courtDetected)
+    print(x)
+    currentTime = time.time() #seconds since 1970
+    print "Movement Detected at " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(currentTime))
+       
+       
 
 #alerts when pin goes high or low
 #adds an event detection for the pin
-GPIO.add_event_detect(pin, GPIO.BOTH, bouncetime=300)
+#enabled pin to trigger an interuption
+GPIO.add_event_detect(pin, GPIO.RISING, bouncetime=300)
 
 #adds an event callback function
 #function assigned to GPIO pin
@@ -66,9 +69,14 @@ GPIO.add_event_callback(pin, callback)
 #While on, run
 while True:
     time.sleep(1)
-
     
-
-
+    #at start of script, this will be vacant
+    #if less than 20 seconds, then 
+    #time.time keeps updating. currentTime is a snapshot.
+    if(currentTime <= (time.time() - 20)) and currentTime != 0:
+        x = requests.post(vacancyDetectedPost, data = courtDetected)
+        print(x.status_code)
+        print "Vacant"
+        currentTime = 0
 
 
